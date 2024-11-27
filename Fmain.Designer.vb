f967519a09f057380ca6e39f69e135bb759e5080
Shayaros <150328065+Shayaros@users.noreply.github.com>
2024-11-27 22:25:48 +0200

    Add files via upload

---
+++ Fmain.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Fmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        tbx1min = New TextBox()
+        tbx1max = New TextBox()
+        tbdx1 = New TextBox()
+        tbx2min = New TextBox()
+        tbx2max = New TextBox()
+        tbdx2 = New TextBox()
+        gv = New DataGridView()
+        btnCalc = New Button()
+        btnClear = New Button()
+        btnExit = New Button()
+        CType(gv, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' tbx1min
+        ' 
+        tbx1min.Location = New Point(685, 72)
+        tbx1min.Name = "tbx1min"
+        tbx1min.Size = New Size(113, 23)
+        tbx1min.TabIndex = 0
+        ' 
+        ' tbx1max
+        ' 
+        tbx1max.Location = New Point(804, 72)
+        tbx1max.Name = "tbx1max"
+        tbx1max.Size = New Size(113, 23)
+        tbx1max.TabIndex = 1
+        ' 
+        ' tbdx1
+        ' 
+        tbdx1.Location = New Point(685, 114)
+        tbdx1.Name = "tbdx1"
+        tbdx1.Size = New Size(113, 23)
+        tbdx1.TabIndex = 2
+        ' 
+        ' tbx2min
+        ' 
+        tbx2min.Location = New Point(685, 152)
+        tbx2min.Name = "tbx2min"
+        tbx2min.Size = New Size(113, 23)
+        tbx2min.TabIndex = 3
+        ' 
+        ' tbx2max
+        ' 
+        tbx2max.Location = New Point(804, 152)
+        tbx2max.Name = "tbx2max"
+        tbx2max.Size = New Size(113, 23)
+        tbx2max.TabIndex = 4
+        ' 
+        ' tbdx2
+        ' 
+        tbdx2.Location = New Point(804, 114)
+        tbdx2.Name = "tbdx2"
+        tbdx2.Size = New Size(113, 23)
+        tbdx2.TabIndex = 5
+        ' 
+        ' gv
+        ' 
+        gv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        gv.Location = New Point(490, 181)
+        gv.Name = "gv"
+        gv.Size = New Size(646, 289)
+        gv.TabIndex = 6
+        ' 
+        ' btnCalc
+        ' 
+        btnCalc.Location = New Point(609, 487)
+        btnCalc.Name = "btnCalc"
+        btnCalc.Size = New Size(121, 81)
+        btnCalc.TabIndex = 7
+        btnCalc.Text = "Розрахувати"
+        btnCalc.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(766, 487)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(121, 81)
+        btnClear.TabIndex = 8
+        btnClear.Text = "Очистити"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(927, 487)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(121, 81)
+        btnExit.TabIndex = 9
+        btnExit.Text = "Вийти"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' Fmain
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1359, 761)
+        Controls.Add(btnExit)
+        Controls.Add(btnClear)
+        Controls.Add(btnCalc)
+        Controls.Add(gv)
+        Controls.Add(tbdx2)
+        Controls.Add(tbx2max)
+        Controls.Add(tbx2min)
+        Controls.Add(tbdx1)
+        Controls.Add(tbx1max)
+        Controls.Add(tbx1min)
+        Name = "Fmain"
+        Text = "Form1"
+        CType(gv, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents tbx1min As TextBox
+    Friend WithEvents tbx1max As TextBox
+    Friend WithEvents tbdx1 As TextBox
+    Friend WithEvents tbx2min As TextBox
+    Friend WithEvents tbx2max As TextBox
+    Friend WithEvents tbdx2 As TextBox
+    Friend WithEvents gv As DataGridView
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+
+End Class
---
+++ Fmain.vb
@@ -0,0 +1,99 @@
+﻿Imports System.Globalization
+Public Class Fmain
+    Private Sub Fmain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles tbx1min.TextChanged
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles gv.CellContentClick
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Використання інваріантної культури для парсингу
+        Dim ci As CultureInfo = CultureInfo.InvariantCulture
+
+        ' Перетворення введених значень
+        Dim x1min As Double = Double.Parse(tbx1min.Text.Replace(",", "."), ci)
+        Dim x1max As Double = Double.Parse(tbx1max.Text.Replace(",", "."), ci)
+        Dim x2min As Double = Double.Parse(tbx2min.Text.Replace(",", "."), ci)
+        Dim x2max As Double = Double.Parse(tbx2max.Text.Replace(",", "."), ci)
+        Dim dx1 As Double = Double.Parse(tbdx1.Text.Replace(",", "."), ci)
+        Dim dx2 As Double = Double.Parse(tbdx2.Text.Replace(",", "."), ci)
+
+        ' Налаштування DataGridView
+        gv.ColumnCount = CInt(Math.Truncate((x2max - x2min) / dx2)) + 2  ' +1 для підсумкового стовпця
+        gv.RowCount = CInt(Math.Truncate((x1max - x1min) / dx1)) + 2     ' +1 для підсумкового рядка
+
+        ' Заповнення заголовків
+        For i As Integer = 0 To gv.RowCount - 2
+            gv.Rows(i).HeaderCell.Value = (x1min + i * dx1).ToString("0.000")
+        Next
+
+        For i As Integer = 0 To gv.ColumnCount - 2
+            gv.Columns(i).HeaderCell.Value = (x2min + i * dx2).ToString("0.000")
+        Next
+
+        gv.Rows(gv.RowCount - 1).HeaderCell.Value = "Сума рядків"
+        gv.Columns(gv.ColumnCount - 1).HeaderCell.Value = "Сума стовпців"
+
+        ' Розрахунок значень
+        Dim rowIdx As Integer = 0
+        Dim x1 As Double = x1min
+        While x1 <= x1max
+            Dim x2 As Double = x2min
+            Dim colIdx As Integer = 0
+            While x2 <= x2max
+                Dim y As Double = x1 + x2  ' Замініть на потрібну функцію
+                gv.Rows(rowIdx).Cells(colIdx).Value = y.ToString("0.000")
+                x2 += dx2
+                colIdx += 1
+            End While
+            x1 += dx1
+            rowIdx += 1
+        End While
+
+        ' Обчислення суми стовпців
+        For col As Integer = 0 To gv.ColumnCount - 2
+            Dim sum As Double = 0
+            For row As Integer = 0 To gv.RowCount - 2
+                sum += Double.Parse(gv.Rows(row).Cells(col).Value, ci)
+            Next
+            gv.Rows(gv.RowCount - 1).Cells(col).Value = sum.ToString("0.000")
+        Next
+
+        ' Обчислення суми рядків
+        For row As Integer = 0 To gv.RowCount - 2
+            Dim sum As Double = 0
+            For col As Integer = 0 To gv.ColumnCount - 2
+                sum += Double.Parse(gv.Rows(row).Cells(col).Value, ci)
+            Next
+            gv.Rows(row).Cells(gv.ColumnCount - 1).Value = sum.ToString("0.000")
+        Next
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Очищення текстових полів
+        tbx1min.Text = ""
+        tbx1max.Text = ""
+        tbx2min.Text = ""
+        tbx2max.Text = ""
+        tbdx1.Text = ""
+        tbdx2.Text = ""
+
+        ' Очищення таблиці
+        gv.Rows.Clear()
+        For cl As Integer = 0 To gv.ColumnCount - 1
+            gv.Columns(cl).HeaderCell.Value = ""
+        Next
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        If MessageBox.Show("Закрити програму?", "Вихід з програми", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) = DialogResult.OK Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
